c68d834d261c55fb27554df21803f32ccddd7099
synexo <synexo@gmail.com>
2015-08-06 21:12:25 +0000

    Commit @ 1438895545

--- public/js/tri_client.js
+++ public/js/tri_client.js
@@ -1,6 +1,7 @@
 function reader() {
     this.viewPages = [];
     this.viewPages.push('1');
+    this.viewPages.push('2');
 };
 
 myReader = new reader();
@@ -8,7 +9,19 @@ myReader = new reader();
 
 var socket = io();
 
+socket.on('connect', function (msg) {
+    console.log('connected');
+});
+
+socket.on('disconnect', function (msg) {
+    console.log('disconnected');
+});
+
 socket.on('message', function (msg) {
+    console.log(JSON.stringify(msg));
+});
+
+socket.on('page', function (msg) {
     console.log(JSON.stringify(msg));
 });
 
